3085c51f7e5300d6f9538a060d3f7cb52c540a14
Julien METTLER <julien.mettler@elca.ch>
2025-12-16 09:31:19 +0100

    [CLOUDTRUST-7617] Add util to get authenticator config

--- kc-cloudtrust-module/kc-cloudtrust-common/src/main/java/io/cloudtrust/keycloak/AuthenticatorUtils.java
+++ kc-cloudtrust-module/kc-cloudtrust-common/src/main/java/io/cloudtrust/keycloak/AuthenticatorUtils.java
@@ -1,5 +1,6 @@
 package io.cloudtrust.keycloak;
 
+import io.cloudtrust.exception.CloudtrustRuntimeException;
 import jakarta.ws.rs.core.MultivaluedHashMap;
 import jakarta.ws.rs.core.MultivaluedMap;
 import org.keycloak.authentication.AuthenticationFlowContext;
@@ -26,5 +27,15 @@ public class AuthenticatorUtils {
     public static String getFirstDecodedFormParameter(AuthenticationFlowContext context, String paramName) {
         List<String> params = getDecodedFormParameters(context, paramName);
         return params == null || params.isEmpty() ? null : params.get(0);
+    }
+
+    public static String getMandatoryConfig(AuthenticationFlowContext context, String configName) {
+        if (context.getAuthenticatorConfig() != null && context.getAuthenticatorConfig().getConfig() != null) {
+            String res = context.getAuthenticatorConfig().getConfig().get(configName);
+            if (res != null && !res.trim().isEmpty()) {
+                return res;
+            }
+        }
+        throw new CloudtrustRuntimeException("Missing configuration " + configName);
     }
 }
--- kc-cloudtrust-module/kc-cloudtrust-common/src/test/java/io/cloudtrust/keycloak/AuthenticatorUtilsTest.java
+++ kc-cloudtrust-module/kc-cloudtrust-common/src/test/java/io/cloudtrust/keycloak/AuthenticatorUtilsTest.java
@@ -1,20 +1,28 @@
 package io.cloudtrust.keycloak;
 
+import io.cloudtrust.exception.CloudtrustRuntimeException;
 import jakarta.ws.rs.core.MultivaluedHashMap;
 import jakarta.ws.rs.core.MultivaluedMap;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.http.HttpRequest;
+import org.keycloak.models.AuthenticatorConfigModel;
 import org.mockito.Mockito;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Stream;
 
 class AuthenticatorUtilsTest {
+    private static final String CONF_NAME = "myConfig";
+    private static final String CONF_VALUE = "myValue";
+
     @ParameterizedTest
     @MethodSource("getRequestSamples")
     void getRequestTest(AuthenticationFlowContext ctx, String httpMethod, String paramName, String expected) {
@@ -45,4 +53,51 @@ class AuthenticatorUtilsTest {
                 Arguments.of(context, "POST", "myParam", "first")
         );
     }
+
+    @Test
+    void getMandatoryConfig_Success() {
+        AuthenticationFlowContext context = Mockito.mock(AuthenticationFlowContext.class);
+        AuthenticatorConfigModel configModel = Mockito.mock(AuthenticatorConfigModel.class);
+
+        Mockito.when(context.getAuthenticatorConfig()).thenReturn(configModel);
+        Mockito.when(configModel.getConfig()).thenReturn(Collections.singletonMap(CONF_NAME, CONF_VALUE));
+
+        String result = AuthenticatorUtils.getMandatoryConfig(context, CONF_NAME);
+        Assertions.assertEquals(CONF_VALUE, result);
+    }
+
+    @Test
+    void getMandatoryConfig_MissingConfig() {
+        AuthenticationFlowContext context = Mockito.mock(AuthenticationFlowContext.class);
+        AuthenticatorConfigModel configModel = Mockito.mock(AuthenticatorConfigModel.class);
+
+        Mockito.when(context.getAuthenticatorConfig()).thenReturn(null);
+        Assertions.assertThrows(CloudtrustRuntimeException.class, () -> AuthenticatorUtils.getMandatoryConfig(context, CONF_NAME));
+
+        Mockito.when(context.getAuthenticatorConfig()).thenReturn(configModel);
+        Mockito.when(configModel.getConfig()).thenReturn(null);
+        Assertions.assertThrows(CloudtrustRuntimeException.class, () -> AuthenticatorUtils.getMandatoryConfig(context, CONF_NAME));
+
+        Mockito.when(context.getAuthenticatorConfig()).thenReturn(configModel);
+        Mockito.when(configModel.getConfig()).thenReturn(Collections.emptyMap());
+        Assertions.assertThrows(CloudtrustRuntimeException.class, () -> AuthenticatorUtils.getMandatoryConfig(context, CONF_NAME));
+    }
+
+    @Test
+    void getMandatoryConfig_EmptyString() {
+        AuthenticationFlowContext context = Mockito.mock(AuthenticationFlowContext.class);
+        AuthenticatorConfigModel configModel = Mockito.mock(AuthenticatorConfigModel.class);
+
+        Map<String, String> config = new HashMap<>();
+
+        config.put(CONF_NAME, "");
+        Mockito.when(context.getAuthenticatorConfig()).thenReturn(configModel);
+        Assertions.assertThrows(CloudtrustRuntimeException.class, () -> AuthenticatorUtils.getMandatoryConfig(context, CONF_NAME));
+
+        config.put(CONF_NAME, "   ");
+        Mockito.when(context.getAuthenticatorConfig()).thenReturn(configModel);
+        Mockito.when(configModel.getConfig()).thenReturn(config);
+        Assertions.assertThrows(CloudtrustRuntimeException.class, () -> AuthenticatorUtils.getMandatoryConfig(context, CONF_NAME));
+    }
+
 }
